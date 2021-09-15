d6dbc8c61a6720466e8eff6ffea7a50b46f156f3
Max <madstk1@pm.me>
2021-09-15 18:42:10 +0200

    feat: help module

---
+++ packages/leon/help.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import utils
+
+def run(string, entities):
+	"""Leon lists what he can do"""
+
+	return utils.output('end', 'help', utils.translate('help'))
